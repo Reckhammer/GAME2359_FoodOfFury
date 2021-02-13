99b4b2e86cef71c1c7a4793bca6f1c007af3d665
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-02-13 10:30:26 -0600

    Update Pickupable.cs
    Pickable: added input check from player

--- FoodsOfFury/Assets/Scripts/Pickupable.cs
+++ FoodsOfFury/Assets/Scripts/Pickupable.cs
@@ -6,28 +6,47 @@ using UnityEngine;
 // Author: Jose Villanueva
 //
 // Description: Allows the ability to add an item to inventory.
-//
-// TODO: add check for button push
 //----------------------------------------------------------------------------------------
 
 public class Pickupable : MonoBehaviour
 {
-    private Item item; // item that will be moved into an inventory
+    private Item item;                  // item that will be moved into an inventory
+    private GameObject player = null;   // target that can pick item up
+    private bool canPickUp = false;     // to check if pickup is ready
 
     void Start()
     {
         item = (Item)gameObject.GetComponent(typeof(Item));
     }
 
-    // needs to be changed to a button push
+    private void Update()
+    {
+        if (canPickUp && Input.GetKeyDown(KeyCode.E))
+        {
+            if (player.GetComponentInParent<Inventory>().add(item))
+            {
+                Destroy(gameObject); // item succesfully added, delete this object
+            }
+        }
+    }
+
+    // player is inside trigger, check for input
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            if (other.GetComponentInParent<Inventory>().add(item))
-            {
-                Destroy(gameObject); // item succesfully added, delete this object
-            }
+            canPickUp = true;
+            player = other.gameObject;
+        }
+    }
+
+    // player left trigger, stop checking for input
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canPickUp = false;
+            player = null;
         }
     }
 }
