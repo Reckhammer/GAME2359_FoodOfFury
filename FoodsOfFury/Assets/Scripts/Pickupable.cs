df6ed1d3e58d9ca25c76d3ed09097f67e5786288
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-02-13 23:04:03 -0600

    Update Pickupable.cs
    Pickapable: added highlight effect

--- FoodsOfFury/Assets/Scripts/Pickupable.cs
+++ FoodsOfFury/Assets/Scripts/Pickupable.cs
@@ -10,19 +10,30 @@ using UnityEngine;
 
 public class Pickupable : MonoBehaviour
 {
-    private Item item;                  // item that will be moved into an inventory
-    private GameObject player = null;   // target that can pick item up
-    private bool canPickUp = false;     // to check if pickup is ready
+    private Item        item;                   // item that will be moved into an inventory
+    private GameObject  player      = null;     // target that can pick item up
+    private Coroutine   highlightCr = null;     // Highlight() coroutine
+    private Renderer    render      = null;     // object renderer
+    private bool        canPickUp   = false;    // to check if pickup is ready
+    private Color       orignal;                // orignal renderer color
 
     void Start()
     {
-        item = (Item)gameObject.GetComponent(typeof(Item));
+        item    = (Item)gameObject.GetComponent(typeof(Item));
+        render  = GetComponent<Renderer>();
+        orignal = render.material.color;
     }
 
     private void Update()
     {
         if (canPickUp && Input.GetKeyDown(KeyCode.E))
         {
+            if (highlightCr != null)
+            {
+                StopCoroutine(highlightCr);         // stop coroutine
+                render.material.color = orignal;    // return to orignal color (before pickup)
+            }
+
             if (player.GetComponentInParent<Inventory>().add(item))
             {
                 Destroy(gameObject); // item succesfully added, delete this object
@@ -37,6 +48,11 @@ public class Pickupable : MonoBehaviour
         {
             canPickUp = true;
             player = other.gameObject;
+
+            if (highlightCr == null) // start highlight coroutine if none exists
+            {
+                highlightCr = StartCoroutine(Highlight(Color.yellow, 1.0f));
+            }
         }
     }
 
@@ -46,7 +62,19 @@ public class Pickupable : MonoBehaviour
         if (other.tag == "Player")
         {
             canPickUp = false;
+            highlightCr = null;
             player = null;
         }
+    }
+
+    // coroutine to lerp objects color (does a highlight effect)
+    private IEnumerator Highlight(Color color, float speed)
+    {
+        while (canPickUp) // do color lerp
+        {
+            render.material.color = Color.Lerp(orignal, color, Mathf.PingPong(Time.time * speed, 1));
+            yield return null;
+        }
+        render.material.color = orignal; // return to original color
     }
 }
