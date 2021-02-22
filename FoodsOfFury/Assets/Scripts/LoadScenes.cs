c92e2892199fbdea68708dd69cdd238f4f9a13eb
Abdon Puente <SoulGrade@users.noreply.github.com>
2021-02-21 18:29:06 -0600

    Initial Menu Scene

---
+++ FoodsOfFury/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+
+    public void FreezerScene()
+    {
+        SceneManager.LoadScene("FreezerLevel");
+    }
+
+    public void KitchenScene()
+    {
+        SceneManager.LoadScene("KitchenLevel");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            SceneManager.LoadScene("MenuScene");
+        }
+
+        if (Input.GetKeyDown("q"))
+        {
+            Application.Quit();
+            Debug.Log("Quit game");
+        }
+
+    }
+
+
+
+}
