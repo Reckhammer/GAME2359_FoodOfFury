c1d7644ee9207410f9fa831139a1c9b84636bbd3
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-02-24 18:19:46 -0600

    Changed how the player moves
    Player_Prototype.prefab: set variables and added health script
    
    Damaging: Now includes an optional knockback
    
    PlayerMovement: Now uses 'AddForce()' instead of setting rigidbody velocity to allow for outside collisions/ velocity changes. Now includes an 'airControll' variable to change player controll while in air. Gliding now stops when grounded (drag is returned to normal). Movement is now clamped (stops faster diagonal speed). Overall organization changes to meet standards.

--- FoodsOfFury/Assets/Scripts/PlayerMovement.cs
+++ FoodsOfFury/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*-------------------------------------------------------
- * Author: Abdon J. Puente IV
+ * Author: Abdon J. Puente IV, Jose Villanueva
  * 
  *  Description: This class handles the players controls
  *  
@@ -11,70 +11,83 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed          = 5f;           // speed if player
+    public float jumpHeight     = 2f;           // jump force of player
+    public float glideRate      = 10f;          // drag rate when gliding
+    public float groundDistance = 0.2f;         // distance to check for ground
+    public LayerMask ground;                    // layers to check if grounded
 
-    public float Speed = 5f;
-    public float JumpHeight = 2f;
-    public float fallRate = 10f;
-    private const int maxJump = 1;
-    private int currentJump = 0;
-    public float GroundDistance = 0.2f;
-    public LayerMask Ground;
+    [Range(0.0f, 1.0f)]
+    public float airControll = 0.5f;            // amount of controll while in air
 
-    private Rigidbody rb;
-    private Vector3 inputs = Vector3.zero;
-    private bool isGrounded = true;
-    private Transform groundChecker;
+    private Rigidbody rb;                       // rigidbody of player
+    private Transform groundChecker;            // position of groundchecker
+    private Vector3 inputs      = Vector3.zero; // inputs from player
+    private float originalDrag  = 1.0f;         // original drag of object
+    private const int maxJump   = 1;            // max amount of jumps
+    private int currentJump     = 0;            // current jump index
+    private bool isGrounded     = true;         // for ground check
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         groundChecker = transform.GetChild(0);
+        originalDrag = rb.drag;
     }
 
-    // Moves the character with arrow keys and AWSD
     void FixedUpdate()
     {
-        Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 camRight = Camera.main.transform.right;
-        Vector3 movement = inputs.z * camForward + inputs.x * camRight;
-        rb.velocity = new Vector3(movement.x * Speed, rb.velocity.y, movement.z * Speed);
+        // get movement based on camera rotation and player inputs
+        Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized; // cam forward without y value
+        Vector3 camRight   = Camera.main.transform.right;
+        Vector3 movement   = inputs.z * camForward + inputs.x * camRight;
+        movement = Vector3.ClampMagnitude(movement, speed); // clamp magnitiude of vector by speed (stops diagonal movement being faster than hor/ver movement)
+        rb.AddForce(movement); // apply movement
     }
-
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
+        inputs = Vector3.zero;
 
-        if (isGrounded == true)
+        // get player horzontal and vertical inputs
+        if (isGrounded) // normal movement
+        {
+            inputs.x = Input.GetAxisRaw("Horizontal") * speed;
+            inputs.z = Input.GetAxisRaw("Vertical") * speed;
+        }
+        else // in air, use air modifier
+        {
+            inputs.x = Input.GetAxisRaw("Horizontal") * speed * airControll;
+            inputs.z = Input.GetAxisRaw("Vertical") * speed * airControll;
+        }
+
+        //if (inputs != Vector3.zero)
+        //transform.forward = inputs;
+
+        // start glide if 'x' is pressed (when not grounded)
+        if (Input.GetKey("x") && !isGrounded)
+        {
+            rb.drag = glideRate;
+            currentJump = 1;
+        }
+
+        // stop glide if 'x' is released or grounded
+        if (Input.GetKeyUp("x") || isGrounded)
+        {
+            rb.drag = originalDrag;
+        }
+
+        if (isGrounded)
         {
             currentJump = 0;
         }
 
-        // X makes the character glide down
-        if (Input.GetKey("x") && (isGrounded == false))
-        {
-            rb.drag = fallRate;
-            currentJump = 1;
-        }
-
-        if (Input.GetKeyUp("x"))
-        {
-            rb.drag = 0;
-        }
-
-        inputs = Vector3.zero;
-        inputs.x = Input.GetAxis("Horizontal");
-        inputs.z = Input.GetAxis("Vertical");
-        //if (inputs != Vector3.zero)
-            //transform.forward = inputs;
-
-        // space bar makes the character jump.
+        // space bar makes the character jump
         if (Input.GetButtonDown("Jump") && (isGrounded || maxJump > currentJump))
         {
-            rb.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
             currentJump++;
         }
-
     }
-
 }
