787cc318d9a8f93af3f56899fe5c1da73b059846
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-03-01 01:11:23 -0600

    Added moving platform basics
    MovingPlatform: Added base class. It allows an object (in layer mask) to move with this object.
    
    MovingPlatform_Prototype.prefab: Basic sctructure of how the platform should be (needs animation)
    
    Player_ProtoType.prefab: Turned off interpolation (causes issued when it is parented). Increased dash force. Set item collider to intended size.

---
+++ FoodsOfFury/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public LayerMask stickTo;
+
+    private void Start()
+    {
+        // if scale is not (1, 1, 1), print warning and set object inactive
+        if (transform.localScale != Vector3.one)
+        {
+            Debug.LogWarning("WARNING: " + gameObject.name + "'s scale needs to be (1, 1, 1) or it will cause scaling issues!!!");
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent == null) // check if parent
+        {
+            if (stickTo == (stickTo | (1 << other.gameObject.layer))) // check if object is in layermask
+            {
+                //print("parenting: " + other.gameObject);
+                other.transform.SetParent(transform); // set parent to parent of this obj
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.parent == transform) // check if this is parent
+        {
+            //print("unparenting: " + other.gameObject);
+            other.transform.SetParent(null);
+        }
+    }
+}
