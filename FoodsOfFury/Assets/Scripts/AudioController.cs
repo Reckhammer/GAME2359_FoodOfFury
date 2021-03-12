e2d71f9d985fb8396054b5d3036bb25bd13d4279
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-03-12 04:22:46 -0600

    Added Audio Implementation
    AudioController: added. Allows to play audio from audio source references that are set up (prefab in Resources folder)
    
    Resources: added folder to hold assets that can be instantiated without a reference (should be limited to objects that don't need a reference only)
    
    TESTING AUDIO.prefab: prefab to showcase how we will set up our audio
    
    AudioObjects: folder for audio objects
    
    MainAudioMixer: main mixer for audio
    
    AudioMixers: folder to hold audio mixers

---
+++ FoodsOfFury/Assets/Scripts/AudioController.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//----------------------------------------------------------------------------------------
+// Author: Jose Villanueva
+//
+// Description: This class acts as an AudioManager. It holds a dictionary of references
+//              to AudioSource's and allows the playing of their audio
+//----------------------------------------------------------------------------------------
+
+public class AudioController : MonoBehaviour
+{
+    private static AudioController instance; // AudioController instance
+
+    private Dictionary<string, AudioSource> records = new Dictionary<string, AudioSource>(); // dictionary of AudioSources by Name
+
+    public AudioSource[] audioInfo; // list of references to be set up
+    
+    // this creates the instance if there is none when accessed
+    public static AudioController Instance
+    {
+        get
+        {
+            if (instance == null) // create from resources folder if accessed but none exists (if moved in folder, this needs to be relinked)
+            {
+                GameObject go = Instantiate(Resources.Load("AudioController", typeof(GameObject))) as GameObject;
+                instance = go.GetComponent<AudioController>();
+                go.name = "AudioController";
+                DontDestroyOnLoad(instance);
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null) // assign as instace if none is
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this) // get rid of copies
+        {
+            Destroy(this);
+        }
+
+        // add AudioInfo to dictionary
+        if (audioInfo != null)
+        {
+            foreach (AudioSource info in audioInfo)
+            {
+                if (info.clip == null) // check if clip is set up
+                {
+                    print("audioInfo at index: " + records.Count + " does not have a clip!");
+                }
+                else if (!records.ContainsKey(info.gameObject.name)) // add new audio source
+                {
+                    records.Add(info.gameObject.name, info);
+                }
+                else // audio source already exists (or the gameObjects have the same name)
+                {
+                    print("Audio list already contains: " + info.gameObject.name);
+                }
+            }
+        }
+    }
+
+    // play audio source from dictionary and return copy
+    public AudioSource play(string clipName, Vector3 position, bool destroyAfterPlay = true)
+    {
+        AudioSource info;
+
+        if (records.TryGetValue(clipName, out info))
+        {
+            GameObject go = Instantiate(info.gameObject); // create copy from dictionary
+            go.transform.position = position;
+
+            AudioSource source = go.GetComponent<AudioSource>();
+            source.Play(); // play audio source
+
+            if (destroyAfterPlay) // destroy after playing clip
+            {
+                Destroy(go, source.clip.length);
+            }
+
+            return source;
+        }
+        else
+        {
+            print("\"" + clipName + "\" was not found in audio list");
+            return null;
+        }
+    }
+
+    // DEBUG: DELETE THIS
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Alpha5))
+    //    {
+    //        printList();
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.Alpha6))
+    //    {
+    //        play("TESTING AUDIO", transform.position);
+    //    }
+    //}
+
+    // DEBUG: print all objects in dictionary
+    public void printList()
+    {
+        foreach (AudioSource info in records.Values)
+        {
+            print(info.gameObject.name);
+        }
+    }
+
+    // release instance on destroy (if this was it)
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+}
