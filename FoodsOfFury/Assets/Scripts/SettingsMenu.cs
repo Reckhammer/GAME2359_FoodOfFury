10c07bbf8b3722a0a1b761b06558109476de8b08
Abdon Puente <54864785+SoulGrade@users.noreply.github.com>
2021-03-20 16:56:10 -0500

    SettingsScene

---
+++ FoodsOfFury/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+ 
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuatity(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+}
