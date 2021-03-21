c135e069fbadfe76ab61eca6c933ac4d941311b2
Abdon Puente <54864785+SoulGrade@users.noreply.github.com>
2021-03-21 16:57:19 -0500

    Update Settings Menu

--- FoodsOfFury/Assets/Scripts/GameManagement/SettingsMenu.cs
+++ FoodsOfFury/Assets/Scripts/GameManagement/SettingsMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -48,10 +49,36 @@ public class SettingsMenu : MonoBehaviour
         currentVolume = volume;
     }
 
-    //Dose not work
+    
     public void SetQuatity(int qualityIndex)
     {
-        QualitySettings.SetQualityLevel(qualityIndex);
+        if(qualityIndex != 6) // if the user is not using any of the presets
+        //{
+            QualitySettings.SetQualityLevel(qualityIndex);
+        //}
+        switch(qualityIndex)
+        {
+            case 0: // quality level - very low
+                textureDropdown.value = 3;
+                break;
+            case 1: // quality level - low
+                textureDropdown.value = 2;
+                break;
+            case 2: // quality level - medium
+                textureDropdown.value = 1;
+                break;
+            case 3: // quality level - high
+                textureDropdown.value = 0;
+                break;
+            case 4: // quality level - very high
+                textureDropdown.value = 0;
+                break;
+            case 5: // quality level - ultra
+                textureDropdown.value = 0;
+                break;
+        }
+
+        qualityDropdown.value = qualityIndex;
     }
 
     public void SetTextureQuality(int textureIndex)
@@ -72,5 +99,45 @@ public class SettingsMenu : MonoBehaviour
         Screen.fullScreen = isFullscreen;
     }
 
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value);
+
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+
+        PlayerPrefs.SetInt("TextureQualityPreference", textureDropdown.value);
+
+        PlayerPrefs.SetInt("FullscreenPreference", Convert.ToInt32(Screen.fullScreen));
+
+        PlayerPrefs.SetFloat("VolumePreference", currentVolume);
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("QualitySettingPreference"))
+            qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
+        else
+            qualityDropdown.value = 3;
+
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            resolutionDropdown.value = currentResolutionIndex;
+
+        if (PlayerPrefs.HasKey("TextureQualityPreference"))
+            textureDropdown.value = PlayerPrefs.GetInt("TextureQualityPreference");
+        else
+            textureDropdown.value = 0;
+
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
+
+        if (PlayerPrefs.HasKey("VolumePreference"))
+            volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+        else
+            volumeSlider.value =PlayerPrefs.GetFloat("VolumePreference");
+    }
 
 }
