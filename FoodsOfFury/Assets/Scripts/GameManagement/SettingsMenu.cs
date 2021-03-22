9279be0da7524b556fab1f6368534e1dc856df84
Abdon Puente <54864785+SoulGrade@users.noreply.github.com>
2021-03-21 19:56:23 -0500

    Settings Update

--- FoodsOfFury/Assets/Scripts/GameManagement/SettingsMenu.cs
+++ FoodsOfFury/Assets/Scripts/GameManagement/SettingsMenu.cs
@@ -22,25 +22,28 @@ public class SettingsMenu : MonoBehaviour
 
     Resolution[] resolutions;
 
-    /* Errors out
     void Start()
     {
-        resolutions = Screen.resolutions;
-
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
 
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string options = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(options);
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
 
     }
-    */
 
     //Controls the audio for MainMixer
     public void SetVolume(float volume)
