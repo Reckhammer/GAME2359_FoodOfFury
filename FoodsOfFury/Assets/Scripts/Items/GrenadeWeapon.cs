ae3d8c3f7722de51b963401861c6554a72fd8809
Abdon Puente <54864785+SoulGrade@users.noreply.github.com>
2021-03-26 20:30:56 -0500

    Update

--- FoodsOfFury/Assets/Scripts/Items/GrenadeWeapon.cs
+++ FoodsOfFury/Assets/Scripts/Items/GrenadeWeapon.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*-------------------------------------------------------
+ * Author: Abdon J. Puente IV
+ * 
+ *  Description: This class handles the player's grenade weapon
+ *  
+ *  */
+
 public class GrenadeWeapon : MonoBehaviour
 {
 
