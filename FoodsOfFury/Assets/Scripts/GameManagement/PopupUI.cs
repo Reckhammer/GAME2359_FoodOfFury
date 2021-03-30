4a27495ead6ed9d11f85bdc445b4436a0108de5e
Abdon Puente <54864785+SoulGrade@users.noreply.github.com>
2021-03-30 17:09:36 -0500

    Freezer Update

---
+++ FoodsOfFury/Assets/Scripts/GameManagement/PopupUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupUI : MonoBehaviour
+{
+
+    public GameObject popupUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popupUI.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            popupUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            popupUI.SetActive(false);
+        }
+    }
+
+
+}
