959a5d0018cc3515035976e74014e4baa06c51b7
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-03-31 17:59:34 -0500

    Item pickup fixes, glide button change (PM2)
    Pickupable: can no longer pickup multiple objects at once
    
    PlayerManager: now has variable to see if in ItemSelection
    
    PlayerMovementTwo: gliding is now spacebar
    
    ConsumablePickup_Prototype.prefab: removed trigger (player already had one and was causing multiple OnTrigger calls)
    
    Player_PM2.prefab: layer changes

--- FoodsOfFury/Assets/Scripts/Items/Pickupable.cs
+++ FoodsOfFury/Assets/Scripts/Items/Pickupable.cs
@@ -14,25 +14,26 @@ public class Pickupable : MonoBehaviour
     public Renderer     render      = null;     // object renderer
 
     private GameObject  player      = null;     // target that can pick item up
-    private Coroutine   highlightCr = null;     // Highlight() coroutine
     private bool        canPickUp   = false;    // to check if pickup is ready
-    private Color       orignal;                // orignal renderer color
+    //private Coroutine   highlightCr = null;     // Highlight() coroutine
+    //private Color       orignal;                // orignal renderer color
 
-    void Start()
-    {
-        orignal = render.material.GetColor("Color_23038745b9c94e5f899a6bcb26c1f301");
-    }
+    //void Start()
+    //{
+    //    orignal = render.material.GetColor("Color_23038745b9c94e5f899a6bcb26c1f301");
+    //}
 
     private void Update()
     {
         if (canPickUp && Input.GetKeyDown(KeyCode.E))
         {
-            if (highlightCr != null)
-            {
-                StopCoroutine(highlightCr);         // stop coroutine
-                render.material.SetColor("Color_23038745b9c94e5f899a6bcb26c1f301", orignal);    // return to orignal color (before pickup)
-            }
+            //if (highlightCr != null)
+            //{
+            //    StopCoroutine(highlightCr);         // stop coroutine
+            //    render.material.SetColor("Color_23038745b9c94e5f899a6bcb26c1f301", orignal);    // return to orignal color (before pickup)
+            //}
 
+            print(player);
             if (player.GetComponentInParent<Inventory>().add(gameObject, type))
             {
                 if (type == ItemType.Consumable)
@@ -43,7 +44,8 @@ public class Pickupable : MonoBehaviour
                 {
                     AudioManager.Instance.playRandom(transform.position, "Rollo_Pickup_01");
                 }
-                player.GetComponentInParent<PlayerManager>().equipNextItem(type); // call player manager to equip new item
+                player.GetComponentInParent<PlayerManager>().equipNextItem(type);  // call player manager to equip new item
+                player.GetComponentInParent<PlayerManager>().itemSelection = null; // set player item selection to null
                 Destroy(gameObject); // item succesfully added, delete this object
             }
         }
@@ -54,13 +56,37 @@ public class Pickupable : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            canPickUp = true;
             player = other.gameObject;
 
-            if (highlightCr == null) // start highlight coroutine if none exists
+            // player has item selected, return
+            if (player.GetComponentInParent<PlayerManager>().itemSelection != null)
             {
-                highlightCr = StartCoroutine(Highlight(Color.yellow, 1.0f));
+                return;
             }
+
+            canPickUp = true;
+            player.GetComponentInParent<PlayerManager>().itemSelection = gameObject;
+
+            //if (highlightCr == null) // start highlight coroutine if none exists
+            //{
+            //    highlightCr = StartCoroutine(Highlight(Color.yellow, 1.0f));
+            //}
+        }
+    }
+
+    // check for pickup while in collider
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // player has item selected, return
+            if (player.GetComponentInParent<PlayerManager>().itemSelection != null)
+            {
+                return;
+            }
+
+            canPickUp = true;
+            player.GetComponentInParent<PlayerManager>().itemSelection = gameObject;
         }
     }
 
@@ -70,24 +96,28 @@ public class Pickupable : MonoBehaviour
         if (other.tag == "Player")
         {
             canPickUp = false;
-            highlightCr = null;
+            //highlightCr = null;
+            if (player.GetComponentInParent<PlayerManager>().itemSelection == gameObject)
+            {
+                player.GetComponentInParent<PlayerManager>().itemSelection = null;
+            }
             player = null;
         }
     }
 
     // coroutine to lerp objects color (does a highlight effect)
-    private IEnumerator Highlight(Color color, float speed)
-    {
-        if (render == null)
-        {
-            yield return null;
-        }
+    //private IEnumerator Highlight(Color color, float speed)
+    //{
+    //    if (render == null)
+    //    {
+    //        yield return null;
+    //    }
 
-        while (canPickUp) // do color lerp
-        {
-            render.material.SetColor("Color_23038745b9c94e5f899a6bcb26c1f301", Color.Lerp(orignal, color, Mathf.PingPong(Time.time * speed, 1)));
-            yield return null;
-        }
-        render.material.SetColor("Color_23038745b9c94e5f899a6bcb26c1f301", orignal); // return to orignal color
-    }
+    //    while (canPickUp) // do color lerp
+    //    {
+    //        render.material.SetColor("Color_23038745b9c94e5f899a6bcb26c1f301", Color.Lerp(orignal, color, Mathf.PingPong(Time.time * speed, 1)));
+    //        yield return null;
+    //    }
+    //    render.material.SetColor("Color_23038745b9c94e5f899a6bcb26c1f301", orignal); // return to orignal color
+    //}
 }
