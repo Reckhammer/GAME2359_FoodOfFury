b3f2c3590eb8f3a6b6243a2c146f66ab7eee8e01
tfv98 <77994498+tfv98@users.noreply.github.com>
2021-04-16 13:11:31 -0500

    Test Cutscene

---
+++ FoodsOfFury/Assets/Scripts/Timeline Scripts/SubtitleBehaviour.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SubtitleBehaviour : PlayableBehaviour
+{
+    public string subtitleText;
+}
