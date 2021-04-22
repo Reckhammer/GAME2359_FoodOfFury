3a2421dc13361c49bd03fc3ed76e785d2c9a8fc4
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-04-21 21:23:02 -0500

    Wave spawner dev
    WaveSpawner: can now spawn enemies on navmesh

--- FoodsOfFury/Assets/Scripts/BossFight/WaveSpawner.cs
+++ FoodsOfFury/Assets/Scripts/BossFight/WaveSpawner.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [System.Serializable]
 public struct enemySpawn
 {
-    public GameObject enemy;
-    public Vector3 position;
-    public Vector3[] waypoints;
+    public GameObject enemyGameobject;  // enemy to be spawned
+    public Transform spawnPosition;     // position for enemy to be spawned
 }
 
 public class WaveSpawner : MonoBehaviour
@@ -16,7 +16,26 @@ public class WaveSpawner : MonoBehaviour
 
     void Start()
     {
-        
+        if (wave1.Length != 0)
+        {
+            for (int x = 0; x <= wave1.Length - 1; x++)
+            {
+                NavMeshHit closestHit; // grab the closest position on navmesh from spawn position to spawn enemy
+                if (NavMesh.SamplePosition(wave1[x].spawnPosition.position, out closestHit, 2.0f, NavMesh.AllAreas))
+                {
+                    GameObject e = Instantiate(wave1[x].enemyGameobject);
+                    e.transform.position = closestHit.position;
+                }
+                else
+                {
+                    print("[wave 1, index: " + x + "], " + wave1[x].enemyGameobject.name + "'s spawn position is not close enough to the navmesh!");
+                }
+            }
+        }
+        else
+        {
+            print("wave 1 is empty!");
+        }
     }
 
     void Update()
