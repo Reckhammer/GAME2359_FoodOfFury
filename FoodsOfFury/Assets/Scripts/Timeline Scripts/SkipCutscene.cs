3393d94441cc8b3764846f4e975c6d783c86bfd2
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-04-29 13:32:30 -0500

    Cutscene cursor/freezing fix
    SkipCutscene: now hides cursor and reverts timescale if it was altered (fixes transition from freezer to kitchen in gameplay)

--- FoodsOfFury/Assets/Scripts/Timeline Scripts/SkipCutscene.cs
+++ FoodsOfFury/Assets/Scripts/Timeline Scripts/SkipCutscene.cs
@@ -8,7 +8,13 @@ public class SkipCutscene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if (Time.timeScale != 1.0f)
+        {
+            Time.timeScale = 1.0f;
+        }
     }
 
     // Update is called once per frame
