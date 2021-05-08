344670105c58f6e074d1a5db79baacb58f6f6f42
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-05-08 03:23:09 -0500

    Loading screen (kitchen cutscene)
    LoadKitchenLevel: can now use loading screen
    
    Cutscene_Kitchen: set up loading screen stuff

--- FoodsOfFury/Assets/Scripts/Timeline Scripts/LoadKitchenLevel.cs
+++ FoodsOfFury/Assets/Scripts/Timeline Scripts/LoadKitchenLevel.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,20 @@ public class LoadKitchenLevel : MonoBehaviour
 {
     void OnEnable()
     {
-        SceneManager.LoadScene("KitchenLevel_1", LoadSceneMode.Single);
+        //SceneManager.LoadScene("KitchenLevel_1", LoadSceneMode.Single);
+        StartCoroutine(Loading("KitchenLevel_1"));
+    }
+
+    private IEnumerator Loading(string level)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(level);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            print("progress: " + progress);
+            UIManager.instance?.setLoadingProgress(progress);
+            yield return null;
+        }
     }
 }
