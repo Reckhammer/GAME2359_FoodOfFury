72ba37c1d6734362ee10f00aeafe9cdcc16ac538
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-05-08 03:02:47 -0500

    Loading screen (Freezer cutscene)
    SkipCutscene: can now loads scene with loading screen
    
    LoadFreezerLevel: can now loads scene with loading screen
    
    Cutscene_Freezer: set up loading screen group and UIManager on canvas

--- FoodsOfFury/Assets/Scripts/Timeline Scripts/SkipCutscene.cs
+++ FoodsOfFury/Assets/Scripts/Timeline Scripts/SkipCutscene.cs
@@ -29,9 +29,21 @@ public class SkipCutscene : MonoBehaviour
     void Skip()
     {
         int index = (SceneManager.GetActiveScene().buildIndex + 1 != SceneManager.sceneCountInBuildSettings) ? SceneManager.GetActiveScene().buildIndex + 1 : 0;
-        SceneManager.LoadScene(index);
+        StartCoroutine(Loading(index));
     }
 
 
+    private IEnumerator Loading(int level)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(level);
 
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            print("progress: " + progress);
+            UIManager.instance?.setLoadingProgress(progress);
+            yield return null;
+        }
+        UIManager.instance?.setLoadingProgress(Mathf.Clamp01(operation.progress / 0.9f));
+    }
 }
