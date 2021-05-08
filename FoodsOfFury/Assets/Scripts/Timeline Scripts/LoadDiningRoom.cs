08881be39cba86805a4e39967b2b5d96e8de547e
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2021-05-08 03:55:55 -0500

    Loading screen dining room cutscene
    LoadDiningRoom: can now use loading screen
    
    Cutscene_DiningRoomFlyover: loading screen set up

--- FoodsOfFury/Assets/Scripts/Timeline Scripts/LoadDiningRoom.cs
+++ FoodsOfFury/Assets/Scripts/Timeline Scripts/LoadDiningRoom.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,20 @@ public class LoadDiningRoom : MonoBehaviour
 {
     void OnEnable()
     {
-        SceneManager.LoadScene("DiningRoomLevel", LoadSceneMode.Single);
+        //SceneManager.LoadScene("DiningRoomLevel", LoadSceneMode.Single);
+        StartCoroutine(Loading("DiningRoomLevel"));
+    }
+
+    private IEnumerator Loading(string level)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(level);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            print("progress: " + progress);
+            UIManager.instance?.setLoadingProgress(progress);
+            yield return null;
+        }
     }
 }
