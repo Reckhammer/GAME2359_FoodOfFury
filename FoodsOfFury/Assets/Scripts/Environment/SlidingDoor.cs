9ca65792b6d00d7e760a5a4f9656fe0c42d4c263
Abdon Puente <54864785+SoulGrade@users.noreply.github.com>
2021-05-12 21:44:38 -0500

    SlidingDoor

---
+++ FoodsOfFury/Assets/Scripts/Environment/SlidingDoor.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoor : MonoBehaviour
+{
+
+    private Animator animator = null;
+    private string doorAnim = null;
+    private bool isClose = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoorAnimations();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isClose == false)
+        {
+
+            isClose = true;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isClose == true)
+        {
+            isClose = false;
+        }
+    }
+
+
+    private void DoorAnimations()
+    {
+        if (isClose == false)
+        {
+            animator.SetBool(doorAnim, false);
+        }
+        else
+        {
+            animator.SetBool(doorAnim, true);
+        }
+    }
+
+    public void setDoorAnim(string anim)
+    {
+        doorAnim = anim;
+    }
+
+    private void OnEnable()
+    {
+        setDoorAnim("PlayerNear");
+    }
+
+}
