4c47ac849127efdc51eeddc062fc2a68cc08ca95
Jose Villanueva <35750552+JoseVillanueva2456@users.noreply.github.com>
2022-05-06 02:42:52 -0500

    Simple audio update for Jose_Testing scene
    nPickupable: now plays audio for "Key", "Starfruit" and "Health" pickups

--- FoodsOfFury/Assets/Scripts/_NewVersions/nPickupable.cs
+++ FoodsOfFury/Assets/Scripts/_NewVersions/nPickupable.cs
@@ -13,6 +13,21 @@ public class nPickupable : MonoBehaviour
 
         if (other.GetComponent<nPlayerInventory>().addItem(GetComponent<nItem>().type))
         {
+            // play pickup sound
+            switch (GetComponent<nItem>().type)
+            {
+                case nItemType.Key:
+                    AudioManager.Instance.play("Pickup_Key_01", transform.position, true);
+                    break;
+                case nItemType.StarFruit:
+                    AudioManager.Instance.play("Pickup_Starfruit_01", transform.position, true);
+                    break;
+                case nItemType.HealthPickup:
+                    AudioManager.Instance.play("Pickup_Health_02", transform.position, true);
+                    break;
+                default:
+                    break;
+            }
             Destroy(this.gameObject);
         }
     }
